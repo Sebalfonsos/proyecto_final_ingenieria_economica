4c352bddf7fbb926a4ef1b0e92025b4d43e3c888
Sebalfonsos <sebastian.sierrat@cecar.edu.co>
2024-05-27 00:39:33 -0500

    smiles in pain

--- js/seriesUniformes/calculadorasAnualidades.js
+++ js/seriesUniformes/calculadorasAnualidades.js
@@ -1070,4 +1070,250 @@ function calcAnualidadAnualidadDiferida() {
     });
 }
 
+function calcVPAnualidadAnticipada() {
+    Swal.fire({
+        title: 'Calculadora de Valor Presente (VP) para Anualidad Anticipada',
+        html: `
+            <h4>Fórmula VP para anualidad anticipada</h4>
+            <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+                <mi>V</mi>
+                <mi>P</mi>
+                <mo>=</mo>
+                <mi>A</mi>
+                <mo>&#xD7;</mo>
+                <mo stretchy="false">(</mo>
+                <mn>1</mn>
+                <mo>+</mo>
+                <mi>i</mi>
+                <mo stretchy="false">)</mo>
+                <mo>&#xD7;</mo>
+                <mfrac>
+                    <mrow>
+                        <mo stretchy="false">(</mo>
+                        <mn>1</mn>
+                        <mo>+</mo>
+                        <mi>i</mi>
+                        <msup>
+                            <mo stretchy="false">)</mo>
+                            <mi>n</mi>
+                        </msup>
+                        <mo>&#x2212;</mo>
+                        <mn>1</mn>
+                    </mrow>
+                    <mrow>
+                        <mi>i</mi>
+                        <mo>&#xD7;</mo>
+                        <mo stretchy="false">(</mo>
+                        <mn>1</mn>
+                        <mo>+</mo>
+                        <mi>i</mi>
+                        <msup>
+                            <mo stretchy="false">)</mo>
+                            <mi>n</mi>
+                        </msup>
+                    </mrow>
+                </mfrac>
+            </math>
+
+            <ul>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>VP</mi>
+                        </mrow>
+                    </math>: Valor presente
+                </li>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>A</mi>
+                        </mrow>
+                    </math>: Monto de cada pago periódico
+                </li>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>i</mi>
+                        </mrow>
+                    </math>: Valor de la tasa de interés por período
+                </li>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>n</mi>
+                        </mrow>
+                    </math>: Número total de períodos
+                </li>
+            </ul>
+
+            <div class="mb-3">
+                <label for="A" class="form-label">Monto de cada pago periódico (A):</label>
+                <input type="number" class="form-control" id="A" step="any">
+            </div>
+
+            <div class="mb-3">
+                <label for="i" class="form-label">Tasa de interés por período (i):</label>
+                <input type="number" class="form-control" id="i" step="any">
+            </div>
+
+            <div class="mb-3">
+                <label for="n" class="form-label">Número total de períodos (n):</label>
+                <input type="number" class="form-control" id="n" step="any">
+            </div>
+        `,
+        showCancelButton: true,
+        confirmButtonText: 'Calcular',
+        preConfirm: () => {
+            const A = parseFloat(document.getElementById('A').value);
+            const i = parseFloat(document.getElementById('i').value);
+            const n = parseFloat(document.getElementById('n').value);
+
+            if (isNaN(A) || isNaN(i) || isNaN(n)) {
+                Swal.showValidationMessage('Por favor, ingrese valores válidos para realizar los cálculos.');
+                return false;
+            }
+
+            const VP = A * (1 + i) * ((1 + i) ** n - 1) / (i * (1 + i) ** n);
+            return VP.toFixed(2);
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Resultado',
+                html: `Valor Presente (VP) calculado para la anualidad anticipada: ${result.value}`,
+                icon: 'success'
+            });
+        }
+    });
+}
+
+function calcMontoAnualidadAnticipada() {
+    Swal.fire({
+        title: 'Calculadora de Monto de Anualidad Anticipada - Usando Valor Presente',
+        html: `
+            <h4>Fórmula A para anualidad anticipada usando VP</h4>
+            <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+                        <mi>A</mi>
+                        <mo>=</mo>
+                        <mfrac>
+                            <mrow>
+                                <mi>V</mi>
+                                <mi>P</mi>
+                            </mrow>
+                            <mrow>
+                                <mo stretchy="false">(</mo>
+                                <mn>1</mn>
+                                <mo>+</mo>
+                                <mi>i</mi>
+                                <mo stretchy="false">)</mo>
+                                <mo>&#xD7;</mo>
+                                <mfrac>
+                                    <mrow data-mjx-texclass="INNER">
+                                        <mo data-mjx-texclass="OPEN">(</mo>
+                                        <mo stretchy="false">(</mo>
+                                        <mn>1</mn>
+                                        <mo>+</mo>
+                                        <mi>i</mi>
+                                        <msup>
+                                            <mo stretchy="false">)</mo>
+                                            <mi>n</mi>
+                                        </msup>
+                                        <mo>&#x2212;</mo>
+                                        <mn>1</mn>
+                                        <mo data-mjx-texclass="CLOSE">)</mo>
+                                    </mrow>
+                                    <mrow>
+                                        <mi>i</mi>
+                                        <mo>&#xD7;</mo>
+                                        <mrow data-mjx-texclass="INNER">
+                                            <mo data-mjx-texclass="OPEN">(</mo>
+                                            <mo stretchy="false">(</mo>
+                                            <mn>1</mn>
+                                            <mo>+</mo>
+                                            <mi>i</mi>
+                                            <msup>
+                                                <mo stretchy="false">)</mo>
+                                                <mrow data-mjx-texclass="ORD">
+                                                    <mi>n</mi>
+
+                                                </mrow>
+                                            </msup>
+                                            
+                                            <mo data-mjx-texclass="CLOSE">)</mo>
+                                        </mrow>
+                                    </mrow>
+                                </mfrac>
+                            </mrow>
+                        </mfrac>
+                    </math>
+
+            <ul>
+                <li>VP: Valor presente</li>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>A</mi>
+                        </mrow>
+                    </math>: Monto de cada pago periódico
+                </li>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>i</mi>
+                        </mrow>
+                    </math>: Valor de la tasa de interés por período
+                </li>
+                <li>
+                    <math xmlns="http://www.w3.org/1998/Math/MathML">
+                        <mrow>
+                            <mi>n</mi>
+                        </mrow>
+                    </math>: Número total de períodos
+                </li>
+            </ul>
+
+            <div class="mb-3">
+                <label for="VP" class="form-label">Valor presente (VP):</label>
+                <input type="number" class="form-control" id="VP" step="any">
+            </div>
+
+            <div class="mb-3">
+                <label for="i" class="form-label">Tasa de interés por período (i):</label>
+                <input type="number" class="form-control" id="i" step="any">
+            </div>
+
+            <div class="mb-3">
+                <label for="n" class="form-label">Número total de períodos (n):</label>
+                <input type="number" class="form-control" id="n" step="any">
+            </div>
+        `,
+        showCancelButton: true,
+        confirmButtonText: 'Calcular',
+        preConfirm: () => {
+            const VP = parseFloat(document.getElementById('VP').value);
+            const i = parseFloat(document.getElementById('i').value);
+            const n = parseFloat(document.getElementById('n').value);
+
+            if (isNaN(VP) || isNaN(i) || isNaN(n) || i <= 0 || n <= 0) {
+                Swal.showValidationMessage('Por favor, ingrese valores válidos para realizar los cálculos.');
+                return false;
+            }
+
+            const A = VP / ((1 + i) * ((Math.pow(1 + i, n) - 1) / (i * Math.pow(1 + i, n))));
+
+            return A.toFixed(2);
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Resultado',
+                html: `Monto de cada pago periódico (A) calculado: ${result.value}`,
+                icon: 'success'
+            });
+        }
+    });
+}
+
+
+
 
